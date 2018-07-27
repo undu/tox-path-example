59fd0f4005fd6497d9ef4436f5a5e5abc1a5e555
Pau Ruiz Safont <unduthegun@gmail.com>
2018-07-27 09:33:56 +0100

    chore: first commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='example',
+    version='1.0.0',
+    description='Example for tox PATH handling',
+
+    author='None',
+    author_email='none@email.com',
+
+    packages=['example'],
+
+    license='GPLv3',
+    classifiers=(
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GPL3 License (GPLv3)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+    )
+)
